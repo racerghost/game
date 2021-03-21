3c4515867851b09530f64b363b4f0bbd4450dfc0
Carlos Bausá Carmona <carlos@MacBook-Pro-de-Carlos.local>
2021-03-22 00:30:52 +0100

    refactoring to classes done

--- js/game.js
+++ js/game.js
@@ -1,63 +1,85 @@
 class Game {
   constructor(options){
     this.ctx=options;
-    this.player = new Player(250,250,options);
-    this.x=250;
-    this.y=250;
+    this.rightPressed = false;
+    this.leftPressed = false;
+    this.spacePressed = false;
+    this.player = new Player(options);
   }
 
-_assignControlsToKeys(){
+//_assignControlsToKeys(key)
 
-    document.addEventListener('keydown',(key)=>{
-        console.log(key.key);
-      switch (key.key) {
-        case "ArrowRight":  
-         // console.log(this.x);
-          this.x=this.x+50      
-          this.player.drawPlayer(this.x,this.y);
-          break;       
-        case "ArrowLeft":        
-          console.log(key.key);
-          break;
-        case "ArrowUp":        
-          console.log(key.key);
-          break;       
-        case "ArrowDown":        
-          console.log(key.key);
-          break;
-        default:
-          break;
-      }
-    })
+ _keyDownHandler=(key)=> {              //Pulso una tecla y miro cual es y asigno valor a su variable
+   if(key.code  == "ArrowRight") {
+     this.rightPressed = true;
+    }
+    else if(key.code == 'ArrowLeft') {
+      this.leftPressed = true;
+    } 
+    else if(key.keyCode===32){
+      this.spacePressed=true;
+    }
   }
 
+  _keyUpHandler=(key)=>{                //suelto una tecla y miro cual es y asigno valor a su variable
+    if(key.code  == "ArrowRight") {
+      this.rightPressed = false;
+    }
+    else if(key.code == 'ArrowLeft') {
+      this.leftPressed = false;
+    }
+    else if(key.keyCode===32){
+      this.spacePressed=false;
+    }
+  }
   
+  _calcPlayer(){
+    this.player.t=this.player.t+0.5;  //Contamos tiempo para calcular la posicion del muñecote
 
-  _drawPlayer(){
+    if(this.player.yp>425){           //Hacemos que no se salga por debajo
+      this.player.vpo=0;
+      this.player.t=0;
+    } 
 
+    if(this.rightPressed) {
+      this.player.ang=85;
+      this.player._recalcAlpha();
+      this.player.xpo += 17;
+      console.log(this.player.ang);
+    }
+    else if(this.leftPressed) {
+      this.player.ang=95;
+      this.player._recalcAlpha();
+      this.player.xpo -= 17;  
+    }
+
+    if(this.spacePressed){
+      console.log('ESPACE:',this.player.ang);
+      this.player.yp=420;
+      this.player.vpo=50;  //velocidad para empujar muñecote hacia arriba
+    } 
+    
+    this.player.yp=Math.trunc(this.player.ypo-((this.player.vpo*Math.sin(this.player.alpha))*this.player.t-(0.5)*this.player.g*this.player.t*this.player.t));
+    this.player.xp=Math.trunc(this.player.xpo+(this.player.vpo*Math.cos(this.player.alpha)));
+    this.player.drawPlayer(this.player.xp,this.player.yp);
     
   }
-
+  
   _clean(){
-      this.ctx.clearRect(0,0,500,500);
+    this.ctx.clearRect(0,0,750,500);
   }
-
-
-
-
+  
   _update(){   
     this._clean();
-    this._assignControlsToKeys();
-    this._drawPlayer();
+    this._calcPlayer();
     window.requestAnimationFrame(this._update.bind(this));
-   }
-
-
-  start(){
- 
-   //console.log(this);
-   //this._update();
-   window.requestAnimationFrame(this._update.bind(this));
   }
+ 
+  start(){
+    
+    document.addEventListener("keydown", this._keyDownHandler, false);
+    document.addEventListener("keyup", this._keyUpHandler, false);
+    window.requestAnimationFrame(this._update.bind(this));
 
+  }
 }
--- js/player.js
+++ js/player.js
@@ -1,23 +1,59 @@
 class Player {
-  constructor(x,y,ctx){
-    this.x=x;
-    this.y=y;
+  constructor(ctx){
     this.ctx=ctx;
-
+    this.t=0;
+    this.xp=0;
+    this.xpo=0;
+    this.yp=425;
+    this.ypo=425;
+    this.xobs=295;
+    this.yobs=395;
+    this.vp=0;
+    this.vpo=0;
+    this.ang=85;
+    this.alpha=(2*this.ang/360)*Math.PI;              /* Calculo angle en radians i a sota els components de la velocitat per x i y */
+    this.g=9.81;
+    this.vx=this.vp*Math.cos(this.alpha);
+    this.vy=this.vp*Math.sin(this.alpha);
+    this.img = new Image();
+    this.img.src = "images/pou.png";
   }
 
-  drawPlayer(x,y){
+  _checkPlayerPostion(){
+    if(this.yp>425){           //Hacemos que no se salga por debajo
+      this.vpo=0;
+      this.vp=0
+      //this.t=0;
+    }
+    
+    if(this.xp<0){            //Hacemos que no se salga por debajo
+      this.xp=-7;
+    }
 
-    this.ctx.fillStyle='green';
-    this.ctx.beginPath();
-    this.ctx.arc(x,y, 40, 0, 2 * Math.PI);
-    this.ctx.stroke();
+    if(this.xp>415){          //Hacemos que no se salga por debajo
+      this.xp=415;
+    }
+    
+  }
+
+  _recalcAlpha(){
+    this.alpha=(2*this.ang/360)*Math.PI;  
+  }
+
+  _reasignInitialValues(){
+    if (this.vpo>0){
+     this.xpo=this.xp;}
+  }
+
+
+
+  drawPlayer(xp,yp){
+    this._checkPlayerPostion();
+    this.ctx.drawImage(this.img,this.xp,this.yp,90,90);
+    this._reasignInitialValues();
   }
 
   moveRight(){
-
-    console.log(this.x);
     this.drawPlayer(this.x,this.y)
-    
   }
 }
